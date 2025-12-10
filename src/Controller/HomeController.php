9835610b31b9de3dcc994f9dae9a73c6181a3624
mahmoud <mahmoudyaya33@gmail.com>
2025-12-10 23:09:24 +0100

    Update HomeContrller and twig for home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\BaieRepository;
+use App\Repository\ClientRepository;
+use App\Repository\UniteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,17 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(
+        BaieRepository $baieRepository,
+        UniteRepository $uniteRepository,
+        ClientRepository $clientRepository
+    ): Response {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'nbBaies' => $baieRepository->count([]),
+            'nbUnites' => $uniteRepository->count([]),
+            'nbClients' => $clientRepository->count([]),
+            'clientsActifs' => $clientRepository->count(['actif' => true]),
+            'clientsInactifs' => $clientRepository->count(['actif' => false]),
         ]);
     }
 }
