ee2dc3dbf217f5533ec899ff99979d2555714333
mahmoud <mahmoudyaya33@gmail.com>
2025-12-15 03:06:35 +0100

    Add userRepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,10 +8,9 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
-/**
- * @extends ServiceEntityRepository<User>
- */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
     public function __construct(ManagerRegistry $registry)
@@ -20,41 +19,92 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     }
 
     /**
-     * Used to upgrade (rehash) the user's password automatically over time.
+     * Sauvegarde un utilisateur dans la base de données 
      */
-    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    public function save(User $entity, bool $flush = false): void
     {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
-        }
+        $this->getEntityManager()->persist($entity);
 
-        $user->setPassword($newHashedPassword);
-        $this->getEntityManager()->persist($user);
-        $this->getEntityManager()->flush();
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
     }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * Supprime un utilisateur de la base de donnée
+     */
+    public function remove(User $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
 
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * Trouve un utilisateur par son email
+     */
+    public function findByEmail(string $email): ?User
+    {
+        return $this->findOneBy(['email' => $email]);
+    }
+
+    /**
+     * Trouve tous les utilisateurs actifs
+     */
+    public function findActifs(): array
+    {
+        return $this->findBy(['actif' => true], ['nom' => 'ASC']);
+    }
+
+    /**
+     * Trouve tous les utilisateurs avec un rôle spécifique
+     */
+    public function findByRole(string $role): array
+    {
+        $qb = $this->createQueryBuilder('u');
+
+        return $qb
+            ->where('JSON_CONTAINS(u.roles, :role) = 1')
+            ->setParameter('role', '"' . $role . '"')
+            ->orderBy('u.nom', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Trouve tous les admins
+     */
+    public function findAdmins(): array
+    {
+        return $this->findByRole('ROLE_ADMIN');
+    }
+
+    /**
+     * Trouve tous les comptables
+     */
+    public function findComptables(): array
+    {
+        return $this->findByRole('ROLE_COMPTABLE');
+    }
+
+    /**
+     * Compte le nombre d'utilisateurs actifs
+     */
+    public function countActifs(): int
+    {
+        return $this->count(['actif' => true]);
+    }
+
+
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $hashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($hashedPassword);
+        $this->save($user, true);
+    }
 }
